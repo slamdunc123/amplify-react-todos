50921b9b61edaa7491983b938fa231709d1e7161
duncanlaidlaw <slamdunc123@gmail.com>
2023-03-22 12:19:49 +0000

    add auth

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
+// @ts-nocheck
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Amplify } from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
